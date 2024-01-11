b82223bbef28a19b9e5134f5d685de3e35b9321e
mkphuthi <mkphuthi@gmail.com>
2024-01-11 14:29:48 -0500

    file cleanup

--- test.py
+++
@@ -1,6 +0,0 @@
-from pathlib import Path
-version_dict = {}
-with open(Path(__file__).parents[0] / 'asimtools/_version.py') as fp:
-    exec(fp.read(), version_dict)
-
-print(version_dict['__version__'])
